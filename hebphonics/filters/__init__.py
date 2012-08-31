6eb76bd98e808fec7dcb787e47243c526b8d349e
The Metaist <metaist@metaist.com>
2012-08-30 22:45:12 -0400

    added: shemot filter for #11

---
+++ hebphonics/filters/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding: utf-8
+
+"""Filters for Hebrew words."""
+
+from .. import metadata
+
+globals().update(metadata.metadata())  # add package metadata
+
+__all__ = ['ShemotFilter']
